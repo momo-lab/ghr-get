2d3e8d4cc0f2d967fd6123a406768b64488c7212
momotaro <momotaro.n@gmail.com>
2023-11-06 10:35:57 +0900

    Add test for match_architecture

---
+++ lib/download-package.sh
@@ -0,0 +1,126 @@
+abort() {
+  {
+    if [ "$#" -eq 0 ]; then
+      cat -
+    else
+      echo "ghr-get: $*"
+    fi
+  } >&2
+  exit 1
+}
+
+function match_patterns() {
+  local files="$1" patterns="$2"
+  local pat
+  for pat in $patterns; do
+    pat="[-_\\.]${pat/\*/[^-_\\.]*}\([-_\\.]\|$\)"
+    local bufs=$(echo "$files" | grep "$pat")
+    if [[ "$bufs" != "" ]]; then
+      files="$bufs"
+    fi
+  done
+  echo "$files"
+}
+
+function match_architecture() {
+  local files="$1"
+  shopt -s nocasematch
+
+  local os
+  case "$(uname)" in
+    mingw* | msys* | cygwin* ) os="windows* win* msvc" ;;
+    linux*) os="linux* musl" ;;
+    darwin) os="darwin osx" ;;
+  esac
+  files=$(match_patterns "$files" "$os")
+
+  local arch
+  case "$(uname -m)" in
+    x86_64) arch="amd64 x86_64 *64" ;;
+    i686) arch="386 i686 *32" ;;
+    # TODO arm not supported
+  esac
+  files=$(match_patterns "$files" "$arch")
+
+  file=$(echo "$files" | head -1)
+  [ -z "$file" ] || echo "$file"
+}
+
+function get_assets() {
+  local site="$1" package="$2" version="$3"
+  local url=https://$site/$package/releases/expanded_assets/$version
+  local html=$(curl -fsSL $url) || abort "Package version '$package@$version' is not found"
+  local files=$(echo "$html" |
+    grep -o "/$package/releases/download/[^\"]*" |
+    awk -F/ '{print $7}')
+  echo "$files"
+}
+
+function get_release_filepath() {
+  local site="$1" package="$2" version="$3"
+  local files=$(get_assets "$site" "$package" "$version")
+  local file=$(match_architecture "$files")
+  [ -z "$file" ] && abort "no target architecture"
+
+  echo "$file"
+}
+
+function download_file() {
+  local url="$1" package_dir="$2"
+
+  curl -sSL -o "$package_dir/$file" "$url"
+  if [ $? -ne 0 ]; then
+    # TODO uninstall package
+    abort "Download error $url"
+  fi
+}
+
+function unarchive_file() {
+  local package="$1" package_dir="$2" file="$3"
+  shopt -s nocasematch
+  case "$file" in
+    *.zip)
+      unzip -oq $package_dir/$file -d $package_dir
+      rm -f $package_dir/$file
+      ;;
+    *.tar.gz|*.tgz)
+      tar -xz -C $package_dir -f $package_dir/$file
+      rm -f $package_dir/$file
+      ;;
+    *)
+      if (file $package_dir/$file | grep "executable" > /dev/null ); then
+        # execution file
+        local newname=${package##*/}
+        local ext=${file##*.}
+        if [ "$ext" != "$file" ]; then
+          newname="$newname.$ext"
+        fi
+        mv $package_dir/$file $package_dir/$newname
+        chmod a+x $package_dir/$newname
+      else
+        echo "Unknown file type. [$file]." >&2
+        return 1
+      fi
+      ;;
+  esac
+}
+
+function download-package() {
+  site="github.com"
+  package="$1"
+  version="$2"
+
+  package_dir="$GHR_GET_ROOT/packages/$site/$package/$version"
+
+  [[ -d "$package_dir" ]] && exit 2
+
+  file="$(get_release_filepath "$site" "$package" "$version")"
+  if [[ $? != 0 ]]; then
+      exit 1
+  fi
+  url="https://$site/$package/releases/download/$version/$file"
+  mkdir -p "$package_dir"
+  download_file "$url" "$package_dir" || exit 1
+  unarchive_file "$package" "$package_dir" "$file" || exit 1
+  echo "$package_dir"
+}
---
+++ spec/download-package_spec.sh
@@ -0,0 +1,206 @@
+Describe 'download-package.sh'
+  Include bin/ghr-get
+  Include lib/download-package.sh
+
+  # default mocked uname command
+  uname() {
+    if [[ "${1:-}" == "-m" ]]; then
+      echo "x86_64"
+    else
+      echo "Linux"
+    fi
+  }
+
+  Describe 'match_patterns function'
+    It 'is no match'
+      files=$(
+        %text
+        #|test_darwin_amd64.zip
+        #|test_darwin_arm64.zip
+        #|test_windows_amd64.zip
+        #|test_windows_arm64.zip
+      )
+      patterns="linux* musl"
+      When call match_patterns "$files" "$patterns"
+      The output should equal "$files"
+    End
+    
+    It 'is match'
+      files=$(
+        %text
+        #|test_linux_amd64.zip
+        #|test_linux_arm64.zip
+        #|test_darwin_amd64.zip
+        #|test_darwin_arm64.zip
+        #|test_windows_amd64.zip
+        #|test_windows_arm64.zip
+      )
+      patterns="linux"
+      When call match_patterns "$files" "$patterns"
+      The lines of output should equal 2
+      The line 1 of output should equal 'test_linux_amd64.zip'
+      The line 2 of output should equal 'test_linux_arm64.zip'
+    End
+  End
+
+  Describe 'match_architecture function'
+    Describe 'for go application'
+      files=$(
+        %text
+        #|test_linux_amd64.zip
+        #|test_linux_arm64.zip
+        #|test_darwin_amd64.zip
+        #|test_darwin_arm64.zip
+        #|test_windows_amd64.zip
+        #|test_windows_arm64.zip
+      )
+      Example 'is linux64'
+        uname() {
+          if [[ "${1:-}" == "-m" ]]; then
+            echo "x86_64"
+          else
+            echo "Linux"
+          fi
+        }
+        When call match_architecture "$files"
+        The output should equal 'test_linux_amd64.zip'
+      End
+
+      Example 'is mingw64'
+        uname() {
+          if [[ "${1:-}" == "-m" ]]; then
+            echo "x86_64"
+          else
+            echo "MINGW64_NT"
+          fi
+        }
+        When call match_architecture "$files"
+        The output should equal 'test_windows_amd64.zip'
+      End
+
+      Example 'is cygwin64'
+        uname() {
+          if [[ "${1:-}" == "-m" ]]; then
+            echo "x86_64"
+          else
+            echo "CYGWIN"
+          fi
+        }
+        When call match_architecture "$files"
+        The output should equal 'test_windows_amd64.zip'
+      End
+
+      Example 'is mac'
+        uname() {
+          if [[ "${1:-}" == "-m" ]]; then
+            echo "x86_64"
+          else
+            echo "DARWIN"
+          fi
+        }
+        When call match_architecture "$files"
+        The output should equal 'test_darwin_amd64.zip'
+      End
+    End
+
+    Describe 'for rust application'
+      files=$(
+        %text
+        #|test-v1.0-arm-unknown-linux-gnueabihf.tar.gz
+        #|test-v1.0-i686-pc-windows-msvc.zip
+        #|test-v1.0-x86_64-apple-darwin.tar.gz
+        #|test-v1.0-x86_64-pc-windows-gnu.zip
+        #|test-v1.0-x86_64-pc-windows-msvc.zip
+        #|test-v1.0-x86_64-unknown-linux-musl.tar.gz
+        #|test_v1.0_amd64.deb
+      )
+      Example 'is linux64'
+        uname() {
+          if [[ "${1:-}" == "-m" ]]; then
+            echo "x86_64"
+          else
+            echo "Linux"
+          fi
+        }
+        When call match_architecture "$files"
+        The output should equal 'test-v1.0-x86_64-unknown-linux-musl.tar.gz'
+      End
+
+      Example 'is mingw64'
+        uname() {
+          if [[ "${1:-}" == "-m" ]]; then
+            echo "x86_64"
+          else
+            echo "MINGW64_NT"
+          fi
+        }
+        When call match_architecture "$files"
+        The output should equal 'test-v1.0-x86_64-pc-windows-msvc.zip'
+      End
+
+      Example 'is cygwin64'
+        uname() {
+          if [[ "${1:-}" == "-m" ]]; then
+            echo "x86_64"
+          else
+            echo "CYGWIN"
+          fi
+        }
+        When call match_architecture "$files"
+        The output should equal 'test-v1.0-x86_64-pc-windows-msvc.zip'
+      End
+
+      Example 'is cygwin32'
+        uname() {
+          if [[ "${1:-}" == "-m" ]]; then
+            echo "i686"
+          else
+            echo "CYGWIN"
+          fi
+        }
+        When call match_architecture "$files"
+        The output should equal 'test-v1.0-i686-pc-windows-msvc.zip'
+      End
+
+      Example 'is mac'
+        uname() {
+          if [[ "${1:-}" == "-m" ]]; then
+            echo "x86_64"
+          else
+            echo "DARWIN"
+          fi
+        }
+        When call match_architecture "$files"
+        The output should equal 'test-v1.0-x86_64-apple-darwin.tar.gz'
+      End
+    End
+  End
+
+  Describe 'get_assets function'
+    It 'is success'
+      site=github.com
+      package=x-motemen/ghq
+      version=v1.4.1
+      When call get_assets "$site" "$package" "$version"
+      The status should equal 0
+      The lines of output should equal 7
+      The line 1 should equal 'ghq_darwin_amd64.zip'
+      The line 2 should equal 'ghq_darwin_arm64.zip'
+      The line 3 should equal 'ghq_linux_amd64.zip'
+      The line 4 should equal 'ghq_linux_arm64.zip'
+      The line 5 should equal 'ghq_windows_amd64.zip'
+      The line 6 should equal 'ghq_windows_arm64.zip'
+      The line 7 should equal 'SHASUMS'
+    End
+  End
+
+  Describe 'get_release_filepath function'
+    It 'is success'
+      site=github.com
+      package=x-motemen/ghq
+      version=v1.4.1
+      When call get_release_filepath "$site" "$package" "$version"
+      The output should equal 'ghq_linux_amd64.zip'
+    End
+  End
+End
